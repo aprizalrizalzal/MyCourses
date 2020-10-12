33d5054cd7fcaa4030c5ab31bb1e84ba2d5395cb
aprizalzal <aprizal040498@gmail.com>
2020-10-12 09:11:41 +0800

    UpdateinUbuntu

---
+++ app/src/main/java/com/application/mycourses/ui/home/activity/meeting/MemberClassActivity.java
@@ -0,0 +1,13 @@
+package com.application.mycourses.ui.home.activity.meeting;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MemberClassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_class);
+    }
+}
